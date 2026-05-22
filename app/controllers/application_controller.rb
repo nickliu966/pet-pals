17ba66355ab3c7a73237adf2a46cc54467ced70c
nickliu966 <nickliu966@gmail.com>
2026-05-22 11:48:41 +0800

    clean up helper method for current user before creating devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  
-  helper_method :current_user
-
-  private
-
-  def current_user
-    @current_user ||= User.first || User.create!(
-      name: "Demo User",
-      email: "demo@example.com",
-      city: "Chicago"
-    )
-  end
 end
