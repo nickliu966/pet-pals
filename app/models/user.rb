70da8100cfbf12edbf822fbcf227389280861f31
nickliu966 <nickliu966@gmail.com>
2026-05-22 10:07:27 +0800

    add devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,20 @@
 #  id         :bigint           not null, primary key
 #  bio        :text
 #  city       :string
-#  email      :string
-#  name       :string
+#  email      :citext
+#  name       :citext
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#  index_users_on_name   (name) UNIQUE
+#
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :pets, dependent: :destroy
 
   has_many :posts, dependent: :destroy
@@ -76,8 +84,8 @@ class User < ApplicationRecord
            source: :walk_event
 
   has_many :friended_by_users,
-        through: :accepted_received_user_friendships,
-        source: :requester
+           through: :accepted_received_user_friendships,
+           source: :requester
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "posts#index"
 
+  devise_for :users
+
   resources :users, only: [:show]
 
   resources :posts
