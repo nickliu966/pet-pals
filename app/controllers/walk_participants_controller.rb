a65f556f6170207ab20bf062e5f7c0f864e7fcea
nickliu966 <nickliu966@gmail.com>
2026-05-27 03:20:35 +0800

    Broadcast walk event participation updates

--- app/controllers/walk_participants_controller.rb
+++ app/controllers/walk_participants_controller.rb
@@ -1,5 +1,5 @@
 class WalkParticipantsController < ApplicationController
-  before_action :set_walk_participant, only: [ :update, :destroy ]
+  before_action :set_walk_participant, only: [:update, :destroy]
 
   def create
     walk_event = WalkEvent.find(walk_participant_params.fetch(:walk_event_id))
@@ -15,6 +15,8 @@ class WalkParticipantsController < ApplicationController
         status: "joined",
         joined_at: Time.current,
       )
+
+      broadcast_walk_event_participation(walk_event)
 
       redirect_to walk_event_path(walk_event),
                   notice: "You joined this walk."
@@ -51,6 +53,8 @@ class WalkParticipantsController < ApplicationController
       notice = "You are already joining this walk."
     end
 
+    broadcast_walk_event_participation(walk_event)
+
     redirect_to walk_event_path(walk_event, return_to: params[:return_to]),
                 notice: notice
   end
@@ -74,7 +78,11 @@ class WalkParticipantsController < ApplicationController
     authorize! @walk_participant
 
     walk_event = @walk_participant.walk_event
+    removed_user = @walk_participant.user
+
     @walk_participant.destroy
+
+    broadcast_walk_event_participation(walk_event, extra_viewers: [removed_user])
 
     redirect_to walk_event_path(walk_event),
                 notice: "Participant was removed."
@@ -89,6 +97,8 @@ class WalkParticipantsController < ApplicationController
       joined_at: Time.current,
     )
 
+    broadcast_walk_event_participation(@walk_participant.walk_event)
+
     redirect_to walk_event_path(@walk_participant.walk_event),
                 notice: "You accepted the invitation."
   end
@@ -97,7 +107,12 @@ class WalkParticipantsController < ApplicationController
     @walk_participant = WalkParticipant.find(params.fetch(:id))
     authorize! @walk_participant
 
+    walk_event = @walk_participant.walk_event
+    viewer = @walk_participant.user
+
     @walk_participant.update!(status: "cancelled")
+
+    broadcast_walk_event_participation(walk_event, extra_viewers: [viewer])
 
     redirect_to walk_event_path(@walk_participant.walk_event),
                 notice: "You declined the invitation."
@@ -105,23 +120,72 @@ class WalkParticipantsController < ApplicationController
 
   private
 
+  def broadcast_walk_event_participation(walk_event, extra_viewers: [])
+    walk_event.reload
+
+    Turbo::StreamsChannel.broadcast_replace_to(
+      walk_event,
+      target: helpers.dom_id(walk_event, :participant_count),
+      partial: "walk_events/participant_count",
+      locals: { walk_event: walk_event },
+    )
+
+    viewers =
+      [
+        walk_event.host_user,
+        *walk_event.confirmed_participants.includes(:user).map(&:user),
+        *extra_viewers,
+      ].compact.uniq
+
+    viewers.each do |viewer|
+      Turbo::StreamsChannel.broadcast_replace_to(
+        participant_list_stream_for(walk_event, viewer),
+        target: helpers.dom_id(walk_event, :participant_list),
+        partial: "walk_events/participant_list",
+        locals: {
+          walk_event: walk_event,
+          viewer: viewer,
+        },
+      )
+
+      Turbo::StreamsChannel.broadcast_replace_to(
+        join_controls_stream_for(walk_event, viewer),
+        target: helpers.dom_id(walk_event, :join_controls),
+        partial: "walk_events/join_controls",
+        locals: {
+          walk_event: walk_event,
+          viewer: viewer,
+          return_to: nil,
+        },
+      )
+    end
+  end
+
+  def participant_list_stream_for(walk_event, viewer)
+    "walk_event_#{walk_event.id}_participant_list_user_#{viewer.id}"
+  end
+
+  def join_controls_stream_for(walk_event, viewer)
+    "walk_event_#{walk_event.id}_join_controls_user_#{viewer.id}"
+  end
+
   def set_walk_participant
     @walk_participant = WalkParticipant.find(params.expect(:id))
   end
 
   def walk_participant_params
-    params.expect(walk_participant: [ :walk_event_id, :pet_id ])
+    params.expect(walk_participant: [:walk_event_id, :pet_id])
   end
 
   def walk_participant_update_params
-    params.expect(walk_participant: [ :status ])
+    params.expect(walk_participant: [:status])
   end
 
   def walk_participant_params
     params.expect(
       walk_participant: [
         :walk_event_id,
-        pet_ids: []
+        pet_ids: [],
       ],
     )
   end
