4e7d07fcaac19671b544deece1e48e9b51378a9d
nickliu966 <nickliu966@gmail.com>
2026-05-26 23:18:39 +0800

    Ajaxify comments with Turbo Streams

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,17 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [ :edit, :update, :destroy ]
+  before_action :set_comment, only: [:edit, :update, :destroy]
 
   def create
     @comment = Comment.new(comment_params)
     @comment.author = current_user
+    @post = @comment.post
+    @expanded = params[:expanded] == "true"
 
     respond_to do |format|
       if @comment.save
         format.html { redirect_back fallback_location: post_path(@comment.post), notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
+        format.turbo_stream
       else
         format.html { redirect_back fallback_location: root_url, alert: @comment.errors.full_messages.to_sentence }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -37,11 +40,15 @@ class CommentsController < ApplicationController
   def destroy
     authorize! @comment
 
+    @post = @comment.post
+    @expanded = params[:expanded] == "true"
+
     @comment.destroy!
 
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Comment was successfully destroyed." }
       format.json { head :no_content }
+      format.turbo_stream
     end
   end
 
@@ -52,6 +59,6 @@ class CommentsController < ApplicationController
   end
 
   def comment_params
-    params.expect(comment: [ :post_id, :body, :parent_comment_id ])
+    params.expect(comment: [:post_id, :body, :parent_comment_id])
   end
 end
