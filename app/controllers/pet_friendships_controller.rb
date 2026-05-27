28ada313e9bbd93916bea11cc9c7a8a539888e24
nickliu966 <nickliu966@gmail.com>
2026-05-27 09:21:45 +0800

    Stream friendship request status updates

--- app/controllers/pet_friendships_controller.rb
+++ app/controllers/pet_friendships_controller.rb
@@ -1,5 +1,5 @@
 class PetFriendshipsController < ApplicationController
-  before_action :set_pet_friendship, only: [ :update, :destroy ]
+  before_action :set_pet_friendship, only: [:update, :destroy]
 
   def index
     user_pet_ids = current_user.pets.select(:id)
@@ -39,10 +39,19 @@ class PetFriendshipsController < ApplicationController
 
     respond_to do |format|
       if @pet_friendship.update(pet_friendship_params)
-        format.html { redirect_back fallback_location: pet_friendships_path, notice: "Pet friendship was successfully updated." }
+        format.html do
+          redirect_back fallback_location: pet_friendships_path,
+                        notice: "Pet friendship was successfully updated."
+        end
+
         format.json { render :show, status: :ok, location: @pet_friendship }
+        format.turbo_stream
       else
-        format.html { redirect_back fallback_location: pet_friendships_path, alert: @pet_friendship.errors.full_messages.to_sentence }
+        format.html do
+          redirect_back fallback_location: pet_friendships_path,
+                        alert: @pet_friendship.errors.full_messages.to_sentence
+        end
+
         format.json { render json: @pet_friendship.errors, status: :unprocessable_entity }
       end
     end
@@ -66,6 +75,6 @@ class PetFriendshipsController < ApplicationController
   end
 
   def pet_friendship_params
-    params.expect(pet_friendship: [ :requester_pet_id, :receiver_pet_id, :status ])
+    params.expect(pet_friendship: [:requester_pet_id, :receiver_pet_id, :status])
   end
 end
